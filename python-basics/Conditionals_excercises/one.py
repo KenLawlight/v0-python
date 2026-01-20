a2007cb370aa1acf9b90c5583d0fa1037df5206d
Ken Lawlight <k3nlawlight@gmail.com>
2026-01-20 15:03:12 +0200

    Conditionals

---
+++ python-basics/Conditionals_excercises/one.py
@@ -0,0 +1,40 @@
+qty = 38
+
+if qty > 30 and qty % 5 == 4:
+    print("swish")
+else:
+    print("swoosh")
+
+if qty > 0:
+    print("pos")
+
+print ("*******************************")
+
+a = "celery"
+b = "SQUASH"
+
+if a == a.upper():
+    print("alpha")
+
+if b == b.upper():
+    print("beta")
+
+print("**************************")
+
+number = 9
+
+if number > 4:
+    print("ding")
+elif number % 3 == 0:
+    print("dong")
+
+print ("*****************************************")
+
+z = 12
+
+if z > 10:
+    print("vroom")
+
+if z % 3 == 0:
+    print("skrrt")
+
