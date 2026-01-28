75f8d2c39b838c509cce6450e87f488c9b1644d4
Ken Lawlight <k3nlawlight@gmail.com>
2026-01-28 16:48:40 +0200

    A_Nested

---
+++ python-basics/Nesting/A_nested_loops_exercise_python/two_sum.py
@@ -0,0 +1,11 @@
+def two_sum(numbers, target):
+    for i in range(len(numbers)):
+        for j in range(i + 1, len(numbers)):
+            if numbers[i] + numbers[j] == target:
+                return True
+    return False
+
+print(two_sum([2, 3, 5, 9], 7))
+print(two_sum([2, 3, 5, 9], 4))
+print(two_sum([6, 3, 4], 10))
+print(two_sum([6, 5, 1], 10))
