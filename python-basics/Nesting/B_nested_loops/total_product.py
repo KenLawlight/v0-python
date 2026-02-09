dd947fea9469a54002527469019b3fdba0ba8ee2
Ken Lawlight <k3nlawlight@gmail.com>
2026-02-09 15:04:04 +0200

    B nested loops

---
+++ python-basics/Nesting/B_nested_loops/total_product.py
@@ -0,0 +1,13 @@
+def total_product(matrix):
+    product = 1
+    for row in matrix:
+        for num in row:
+            product *= num
+    return product
+
+array1 = [[3, 5, 2], [6, 2]]
+array2 = [[4, 6], [2, 3], [1, 2]]
+
+print(total_product(array1))  # 360
+print(total_product(array2))  # 288
+
